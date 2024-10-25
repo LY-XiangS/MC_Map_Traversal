cd4bd4b60f7d48a51a392d9487c3e540e0c7a15a
hfsz2319 <hfsz2319_1@outlook.com>
2024-10-25 14:47:54 +0800

    update check

--- src/check.py
+++ src/check.py
@@ -1,18 +1,44 @@
-def check(
-    start: str, mode: int, arg1: str, arg2: str | None = None
-) -> str:
-    if start == "":
+def check0(arg: str) -> str:
+    if arg == "":
         return "请先输入地图画的编号!"
-    elif not start.isnumeric():
-        return "请输入正确的地图画编号!"
-    elif start := int(start) < 0:
-        return "地图画编号不得为负!"
-
-    if mode == 1:
-        pass
-    elif mode == 2:
-        pass
-    elif mode == 3:
-        pass
+    elif not arg.isnumeric() or int(arg) < 0:
+        return "地图画编号应为非负整数!"
     else:
-        raise ValueError("mode:int")
+        return str(int(arg))
+
+
+def check1(arg: str) -> str:
+    if arg == "":
+        return "请输入地图画遍历的数量!"
+    elif not arg.isdecimal() or int(arg) <= 0:
+        return "地图画数量应为正整数!"
+    else:
+        return str(int(arg))
+
+
+def check2(arg1: str, arg2: str) -> str:
+    if arg1 == "" or arg2 == "":
+        return (
+            "请输入地图画的"
+            + ("长" if arg1 == "" else "")
+            + ("宽" if arg2 == "" else "")
+            + ("" if arg1 == "" and arg2 == "" else "度")
+        )
+
+    elif not arg1.isdecimal() or not arg2.isdecimal() or int(arg1) * int(arg2) <= 0:
+        return "地图画面积应为正整数!"
+    else:
+        return str(int(arg1) * int(arg2))
+
+
+def check3(arg1: str, arg2: str) -> str:
+    if arg1 == "" or arg2 == "":
+        return (
+            "请输入"
+            + ("" if arg1 == "" and arg2 == "" else "第")
+            + ("一" if arg1 == "" else "")
+            + ("二" if arg2 == "" else "")
+            + "幅地图画的编号!"
+        )
+    elif not arg1.isdecimal() or not arg2.isdecimal() or int(arg2) - int(arg1) <= 0:
+        return "地图画之差应为正整数!"
